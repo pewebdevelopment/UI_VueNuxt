0b75863901c2d1bd21bee4ba8143bbde4a8bf019
Bhavya Tiwari <109498434+bhavyatiwarigithub@users.noreply.github.com>
2023-03-22 11:12:50 +0530

    Setup Nuxt

---
+++ nuxt-UI_Collection/Components/Demo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+Hello Demo
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt-UI_Collection/Pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <DEMO />
+</template>
+
+<script setup>
+import DEMO from '@/Components/Demo.vue';
+
+</script>
+
+<style scoped></style>
